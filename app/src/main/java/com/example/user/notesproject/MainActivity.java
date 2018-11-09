ef49b6ae1c0898e3f54eb42557b3852fb3bca16c
Hfragoso <heberfm@hotmail.com>
2018-11-09 06:06:00 -0800

    Configured databinding to the project

--- app/src/main/java/com/example/user/notesproject/MainActivity.java
+++ app/src/main/java/com/example/user/notesproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.user.notesproject;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.user.notesproject.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String myText = "Hello binding";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setMyData(this);
+        myText = "data changed";
+        binding.notifyChange();
     }
 }
